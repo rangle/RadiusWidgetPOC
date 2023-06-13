776af9d2aa30e00e201115db5443b4225f42ae3a
Bohdan Anderson <bohdan.anderson@rangle.io>
2023-06-13 15:23:22 -0400

    testing out ordering of components

--- basicTokenStudio/widget-src/code.tsx
+++ basicTokenStudio/widget-src/code.tsx
@@ -4,18 +4,31 @@ const { AutoLayout, useSyncedState, Text } = widget
 import { TokenSets, TokensAsObject } from './types'
 import { RenderListOfTokens } from './renderListOfTokens';
 
+// check figma.root for values
+// if there is no values make a default value
+const SetDefaultValues = () => {
+  const usedTokenSet = JSON.parse(figma.root.getSharedPluginData("tokens", "values"))as TokenSets;
+  console.log(usedTokenSet)
+  if (Object.keys(usedTokenSet).length === 0) {
+    figma.notify("No tokens found, creating default tokens");
+    figma.root.setSharedPluginData("tokens", "values", JSON.stringify({
+      "global": []
+    }));
+  }
+}
+
+
 // TODO add computed value, this is for math and reference props
 const GetAllTokens = (setTokens:Function) => {
   console.log("GetAllTokens");
 
   const usedTokenSet = JSON.parse(figma.root.getSharedPluginData("tokens", "usedTokenSet"));
   const activeSets = Object.keys(usedTokenSet).filter((key) => usedTokenSet[key] === "enabled");
-  // console.log(activeSets);
-
   const allTokenValues = JSON.parse(figma.root.getSharedPluginData("tokens", "values")) as TokenSets;
-  
+
   const activeTokens = activeSets
     .map((set) => allTokenValues[set])
+
 
   const noDuplicates = activeTokens.reduce((acc, val) => {
     val.forEach((token) => {
@@ -30,13 +43,19 @@ const GetAllTokens = (setTokens:Function) => {
 
 function Widget() {
   const [tokens, setTokens] = useSyncedState<TokensAsObject>('tokens',{})
-
   return <AutoLayout 
     stroke={'#000'} 
     padding={10} 
     direction="vertical"
     spacing={10}
     >
+    <AutoLayout
+      onClick={()=>SetDefaultValues()}
+      padding={5}
+      fill={'#ccc'}
+      >
+      <Text >Click to set default values</Text>
+    </AutoLayout>
     <AutoLayout 
       onClick={()=>GetAllTokens(setTokens)} 
       direction="horizontal"
--- tokenSets/widget-src/code.tsx
+++ tokenSets/widget-src/code.tsx
@@ -1,8 +1,44 @@
 const { widget } = figma
-const { Frame } = widget
+const { AutoLayout, Text, useSyncedState } = widget
+
+type UsedTokenSet = {[key:string]:'enabled'|'disabled'}
+
+const getTokenSets = (setValue:(newVal:string)=>void) => {
+  const tokenSets = JSON.parse(figma.root.getSharedPluginData("tokens", "usedTokenSet")) as UsedTokenSet;
+  setValue(JSON.stringify(tokenSets))
+}
+
+const setTokenSets = () => {
+  const tokenSets = (JSON.parse(figma.root.getSharedPluginData("tokens", "usedTokenSet")) as UsedTokenSet);
+  Object.keys(tokenSets).forEach((key) => {
+    if(key ==='global') return
+    tokenSets[key] = 'disabled'
+  });
+  // const tmp = {...tokenSets['style-1']};
+  figma.root.setSharedPluginData("tokens", "usedTokenSet", JSON.stringify(tokenSets));
+}
+
 
 function Widget() {
-  return <Frame width={100} height={100} fill={'#C4C4C4'}></Frame>
+  const [tokens, setTokens] = useSyncedState<string>('tokens','hello world')
+
+  return <AutoLayout 
+    direction={'vertical'}
+    fill={'#C4C4C4'}
+    padding={8} 
+    >
+    <Text
+      onClick={()=>getTokenSets(setTokens)}
+    >
+      Get Tokens Sets
+    </Text>
+    <Text onClick={setTokenSets}>
+      Disable all token sets
+    </Text>
+    <Text>
+      {tokens}
+    </Text>
+  </AutoLayout>
 }
 
 widget.register(Widget)
