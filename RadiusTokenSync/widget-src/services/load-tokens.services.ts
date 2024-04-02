125d481d351322c3a00bd270c0f8891dd91d3cb7
Jason Santos <jason.santos@rangle.io>
2024-04-02 15:56:16 -0400

    Adds functions to push data to Github and load variables from Figma

--- RadiusTokenSync/widget-src/services/load-tokens.services.ts
+++ RadiusTokenSync/widget-src/services/load-tokens.services.ts
@@ -1,5 +1,23 @@
 // reads current version of tokens and variables of current document
 
+import { generateLayerFile } from "../common/generator.utils";
+import {
+  TokenCollection,
+  getAllLocalVariableTokens,
+} from "../common/variables.utils";
+
+export const getTokenLayers = async () => {
+  const tokenCollections = await getAllLocalVariableTokens();
+  const errors = validateTokenCollection(tokenCollections);
+  if (errors.length) return [tokenCollections, undefined, errors] as const;
+  const layers = generateLayerFile(tokenCollections);
+  return [tokenCollections, layers, errors] as const;
+};
+
 // compare two versions of tokens, returning non-breaking changes and breaking changes
 
 // validate tokens, returning list of errors
+export const validateTokenCollection = (_collections: TokenCollection[]) => {
+  // TODO: add validation
+  return [];
+};
